fdbe45af89ffc7c415be8e1393289e7b6f7956df
Mani <realmanisingh@gmail.com>
2020-09-10 18:51:19 -0400

    Updating: Implemented school component with height parameter

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,21 @@
-def draw_school():
-    print("school not found")
-    return
+def draw_school(height=0):
+    print("        ^")
+    print("       / \\")
+    print("        - ")
+    print("       |O|")
+    print("       | |")
+    print(" ------   ------")
+    print("/                \\")
+    print("______SCHOOL______")
+    print(" |              |")
+    print(" | [ ]      [ ] |")
+    
+    for i in range(height):
+        if (i + 1) % 2 == 0 and i != 0:
+            print(" | [ ]      [ ] |")
+        else:
+            print(" |              |")
+        
+    print(" |      ___     |")
+    print(" |      | |     |")
+    print(" |      |_|     |")
