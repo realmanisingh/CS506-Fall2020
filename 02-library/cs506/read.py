27ef302fda8f0f50b7af5309408ad71e414b358e
Mani <realmanisingh@gmail.com>
2020-09-17 12:26:17 -0400

    Implemented library function

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,19 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, 'r') as f:
+        data = f.readlines()
+        for line in data:
+            line = line.split(",")
+            line[1] = line[1].rstrip("\n")
+            int_list = []
+            for text in line:
+                try:
+                    int_list.append(int(text))
+                except ValueError:
+                    int_list.append(text.strip('\"'))
+                    
+            matrix.append(int_list)
+            
+    return matrix 
